6713ff29c001ee75d4be43efa00fb0ce231fe1bf
Hugo <hugocarbiener@gmail.com>
2021-12-01 11:32:31 +0100

    Add character Model

---
+++ projet-AMJV/Assets/Scripts/CharacterModel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class CharacterModel
+{ 
+    private Classes CharacterClass;
+
+    public abstract void MainSpell();
+    public abstract void SecondarySpell();
+    public abstract void MovementSpell();
+
+}
