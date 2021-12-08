0a7ee8b2acdcc66b44c33f048e197bf0a04c7fc1
Solene <suchuku35@gmail.com>
2021-12-08 09:45:41 +0100

    Progress on fosse tile prefab
    Script is done ; has to be put on the fosse tile prefab

---
+++ projet-AMJV/Assets/Scripts/FosseManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FosseManager : MonoBehaviour
+{
+    [SerializeField] private int deathTime;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //on part du principe que les heros et les monstres ont une variable "isPushed"
+
+        tag = collision.gameObject.tag;
+
+        if (tag == "Player")
+        {
+            if(collision.gameObject.GetComponent<PlayerManager>().isPushed == true)
+            {
+                collision.gameObject.GetComponent<PlayerManager>().isPushed = false;
+
+                //on attend un peu avant de tuer le joueur
+                StartCoroutine(timer(deathTime));
+                StopCoroutine(timer(deathTime));
+
+                Destroy(collision.gameObject);
+            }
+
+        }
+
+        if (tag == "Monster")
+        {
+            if (collision.gameObject.GetComponent<MonsterManager>().isPushed == true)
+            {
+                collision.gameObject.GetComponent<MonsterManager>().isPushed = false;
+
+                StartCoroutine(timer(deathTime));
+                StopCoroutine(timer(deathTime));
+
+                Destroy(collision.gameObject);
+            }
+        }
+
+    }
+
+    IEnumerator timer(int secs)
+    {
+        yield return new WaitForSeconds(secs);
+    }
+}
