3bf78cae0fb62c23660714d68bff439ac3955e06
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2021-12-14 12:25:02 +0100

    Paramètres terminés

--- projet-AMJV/Assets/Scripts/MainMenu.cs
+++ projet-AMJV/Assets/Scripts/MainMenu.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
@@ -11,6 +14,12 @@ public class MainMenu : MonoBehaviour
     [SerializeField] Slider mainVolumeSlider;
     [SerializeField] Slider musicVolumeSlider;
     [SerializeField] Slider effectsVolumeSlider;
+    [SerializeField] TMPro.TMP_Dropdown resolutionDropdown;
+    [SerializeField] TMPro.TMP_Dropdown qualityDropdown;
+    [SerializeField] AudioMixer mixer;
+    string masterVolume = "MasterVolume";
+    string musicVolume = "MusicVolume";
+    string soundEffectsVolume = "SoundEffectsVolume";
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +28,23 @@ public class MainMenu : MonoBehaviour
         parametersMenu = GameObject.Find("ParametersContainer");
         characterSelect.SetActive(false);
         parametersMenu.SetActive(false);
+        if (mainVolumeSlider != null && PlayerPrefs.HasKey("volume"))
+        {
+            float wantedMainVolume = PlayerPrefs.GetFloat("mainVolume", 0);
+            float wantedMusicVolume = PlayerPrefs.GetFloat("musicVolume", 0);
+            float wantedEffectsVolume = PlayerPrefs.GetFloat("effectsVolume", 0);
+            mainVolumeSlider.value = wantedMainVolume;
+            musicVolumeSlider.value = wantedMusicVolume;
+            effectsVolumeSlider.value = wantedEffectsVolume;
+            AudioListener.volume = wantedMainVolume;
+            mixer.SetFloat(masterVolume, wantedMainVolume);
+            mixer.SetFloat(musicVolume, wantedMusicVolume);
+            mixer.SetFloat(soundEffectsVolume, wantedEffectsVolume);
+            mainVolumeSlider.onValueChanged.AddListener(delegate { SelectGeneralVolume(); });
+            mainVolumeSlider.onValueChanged.AddListener(delegate { SelectMusicVolume(); });
+            mainVolumeSlider.onValueChanged.AddListener(delegate { SelectSoundEffectsVolume(); });
+
+        }
     }
 
     public void GoToCharacterSelect()
@@ -31,6 +57,16 @@ public class MainMenu : MonoBehaviour
     {
         parametersMenu.SetActive(true);
         mainMenu.SetActive(false);
+        float mainVolume;
+        mixer.GetFloat(masterVolume, out mainVolume);
+        mainVolumeSlider.value = mainVolume;
+        float mVolume;
+        mixer.GetFloat(musicVolume, out mVolume);
+        musicVolumeSlider.value = mVolume;
+        float effectsVolume;
+        mixer.GetFloat(soundEffectsVolume, out effectsVolume);
+        effectsVolumeSlider.value = effectsVolume;
+
     }
 
     public void BackToMainMenuFromCharSelect()
@@ -53,11 +89,80 @@ public class MainMenu : MonoBehaviour
 
     public void SelectGeneralVolume()
     {
-        AudioListener.volume = mainVolumeSlider.value;
+        mixer.SetFloat(masterVolume, mainVolumeSlider.value);
     }
 
     public void SelectMusicVolume()
     {
+        mixer.SetFloat(musicVolume, musicVolumeSlider.value);
+    }
 
+    public void SelectSoundEffectsVolume()
+    {
+        mixer.SetFloat(soundEffectsVolume, effectsVolumeSlider.value);
+    }
+
+    public void SelectResolution()
+    {
+        if (resolutionDropdown.value == 0)
+        {
+            Screen.SetResolution(1680, 1050, true);
+            Debug.Log("1680x1050");
+        }
+        if (resolutionDropdown.value == 1)
+        {
+            Screen.SetResolution(1440, 900, true);
+            Debug.Log("1440x900");
+        }
+        if (resolutionDropdown.value == 2)
+        {
+            Screen.SetResolution(1280, 800, true);
+            Debug.Log("1280x800");
+        }
+    }
+
+    public void SelectQuality()
+    {
+        if (qualityDropdown.value == 0)
+        {
+            QualitySettings.SetQualityLevel(5, true);
+            int qualityLevel = QualitySettings.GetQualityLevel();
+            Debug.Log(qualityLevel);
+        }
+        if (qualityDropdown.value == 1)
+        {
+            QualitySettings.SetQualityLevel(4, true);
+            int qualityLevel = QualitySettings.GetQualityLevel();
+            Debug.Log(qualityLevel);
+        }
+        if (qualityDropdown.value == 2)
+        {
+            QualitySettings.SetQualityLevel(3, true);
+            int qualityLevel = QualitySettings.GetQualityLevel();
+            Debug.Log(qualityLevel);
+        }
+        if (qualityDropdown.value == 3)
+        {
+            QualitySettings.SetQualityLevel(2, true);
+            int qualityLevel = QualitySettings.GetQualityLevel();
+            Debug.Log(qualityLevel);
+        }
+        if (qualityDropdown.value == 4)
+        {
+            QualitySettings.SetQualityLevel(1, true);
+            int qualityLevel = QualitySettings.GetQualityLevel();
+            Debug.Log(qualityLevel);
+        }
+        if (qualityDropdown.value == 5)
+        {
+            QualitySettings.SetQualityLevel(0, true);
+            int qualityLevel = QualitySettings.GetQualityLevel();
+            Debug.Log(qualityLevel);
+        }
+    }
+
+    public void StartOnClick()
+    {
+        SceneManager.LoadScene("test");
     }
 }
